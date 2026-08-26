6ec246a8ebddd11d8cbd9e022bc8c57784c8ffa4
Abhinav Karande <karandeabhinavcse@gmail.com>
2026-08-26 11:40:12 +0530

    Day 01: Start Go practice

---
+++ Hellow.go
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+package main
+import"fmt"
+
+func main()  {
+    
+    fmt.Print("Hello World")
+=======
+package main
+import"fmt"
+
+func main()  {
+    
+    fmt.Print("Hello World")
+>>>>>>> 27b5f57b07d4a67943852237723dd07e59a4c6d9
+}
---
+++ Variable Declaration.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	var Student1 string = "Super"
+	var Student2 string = "user"
+	x := 2
+
+	fmt.Print(Student1)
+	fmt.Print(Student2)
+	fmt.Print(x)
+}

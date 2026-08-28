91c22c4c713b6ccef284fac3a6c26937e59f13dd
Abhinav8866 <karandeabhinav@gmail.com>
2026-08-28 11:13:00 +0530

    Daily Go practice

---
+++ Declaring Constant.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+const PI = 3.44
+
+func main(){
+	
+	fmt.Println(PI)
+}
---
+++ Multiple Constants Declaration.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	a int = 1
+	b     = 3.16
+	c     = "hi"
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
